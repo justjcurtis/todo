cb448d55f708c15d6555fe2e5add217277d569ea
justjcurtis <justjcurtis@gmail.com>
2023-12-10 17:19:46 +0000

    chore: remove console log

--- todo-client/src/utils/password.js
+++ todo-client/src/utils/password.js
@@ -3,6 +3,5 @@ export const checkPassword = (password) => {
     const has2Capitals = !!password.match(/[A-Z].*[A-Z]/);
     const has2Lowercase = !!password.match(/[a-z].*[a-z]/);
     const has1Symbol = !password.match(/^[a-zA-Z0-9]*$/);
-    console.log(has2Numbers, has2Capitals, has2Lowercase, has1Symbol, password.length >= 8)
     return has2Numbers && has2Capitals && has2Lowercase && has1Symbol && password.length >= 8;
 }
