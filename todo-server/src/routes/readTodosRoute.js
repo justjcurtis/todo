4231ae11717ec6e477d47553422b197ea34d553a
justjcurtis <justjcurtis@gmail.com>
2023-12-10 20:09:00 +0000

    feat: add pagination to readTodosRoute

--- todo-server/src/routes/readTodosRoute.js
+++ todo-server/src/routes/readTodosRoute.js
@@ -1,7 +1,14 @@
 const TodoModel = require('../models/todoModel');
 
 const readTodosRoute = async (req, res) => {
-    const todos = await TodoModel.find({ userId: req.user.id });
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 10;
+    const skip = (page - 1) * limit;
+    const query = TodoModel.find({ userId: req.user.id });
+    query.skip(skip).limit(limit);
+    const countQuery = await TodoModel.countDocuments({ userId: req.user.id });
+    const todos = await query;
+    res.set('X-Total-Count', countQuery);
     res.json(todos);
 }
 
