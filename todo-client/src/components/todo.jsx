2fa4e71250961cb97adf4f2dfc0e3889e4c21fb7
justjcurtis <justjcurtis@gmail.com>
2023-12-10 22:45:24 +0000

    fix: can now change todo value on front end

--- todo-client/src/components/todo.jsx
+++ todo-client/src/components/todo.jsx
@@ -11,7 +11,7 @@ export const Todo = ({ text, completed, onChange, onToggleCompleted, onDelete })
     return (
         <div className='flex'>
             <input onClick={onToggleCompleted} type="checkbox" defaultChecked={completed ? "checked" : ""} className="checkbox checkbox-accent my-auto" />
-            <input onBlur={handleOnBlur} onChange={(e) => setValue(e)} value={value} type="text" className="input input-bordered w-full mx-5 my-1" />
+            <input onBlur={handleOnBlur} onChange={(e) => setValue(e.target.value)} value={value} type="text" className="input input-bordered w-full mx-5 my-1" />
             <button onClick={onDelete} className="btn btn-circle my-auto">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
             </button>
