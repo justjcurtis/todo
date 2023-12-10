14f8936a49fb99d1c5bbed6fad988a1bd6d440a1
justjcurtis <justjcurtis@gmail.com>
2023-12-10 19:42:45 +0000

    feat: implement logout

--- todo-client/src/components/navbar.jsx
+++ todo-client/src/components/navbar.jsx
@@ -1,4 +1,8 @@
+import { useUserContext } from '../hooks/useUserContext'
+
 export const Navbar = () => {
+    const { isLoggedIn, logout } = useUserContext()
+    const showLogin = !isLoggedIn()
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
@@ -6,7 +10,9 @@ export const Navbar = () => {
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a href="/#/login">Login / Sign up</a></li>
+                    {showLogin && <li><a href="/#/login">Login / Sign up</a></li>}
+                    {!showLogin && <li><a onClick={logout}>Logout</a></li>}
+
                 </ul>
             </div>
         </div>
