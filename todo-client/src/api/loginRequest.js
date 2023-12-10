34f0f0ec26ee0ef190ba183f0717810a8b0e7327
justjcurtis <justjcurtis@gmail.com>
2023-12-10 18:27:40 +0000

    feat: add login request

---
+++ todo-client/src/api/loginRequest.js
@@ -0,0 +1,15 @@
+import { API_URL } from '../constants';
+export const loginRequest = async (email, password) => {
+    const response = await fetch(`${API_URL}/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+    });
+    const data = await response.json();
+    if (response.ok) {
+        return data;
+    }
+    throw new Error(data.error);
+}
