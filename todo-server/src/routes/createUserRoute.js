6af0ed124e997ede162f40ed41875a71593c5f2f
justjcurtis <justjcurtis@gmail.com>
2023-12-10 17:11:28 +0000

    fix: password symbol check

--- todo-server/src/routes/createUserRoute.js
+++ todo-server/src/routes/createUserRoute.js
@@ -5,7 +5,7 @@ const checkPassword = (password) => {
     const has2Numbers = password.match(/[0-9].*[0-9]/);
     const has2Capitals = password.match(/[A-Z].*[A-Z]/);
     const has2Lowercase = password.match(/[a-z].*[a-z]/);
-    const has1Symbol = !password.match(/[a-zA-Z0-9 ]*/);
+    const has1Symbol = !password.match(/^[a-zA-Z0-9]*$/);
     return has2Numbers && has2Capitals && has2Lowercase && has1Symbol && password.length >= 8;
 }
 
