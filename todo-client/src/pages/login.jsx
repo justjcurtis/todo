6b6a048a2c0daaea4c8253d8b4bada331755162b
justjcurtis <justjcurtis@gmail.com>
2023-12-10 17:20:59 +0000

    feat: add validation to login

--- todo-client/src/pages/login.jsx
+++ todo-client/src/pages/login.jsx
@@ -1,4 +1,17 @@
+import { useState } from 'react'
+import { checkPassword } from '../utils/password'
+
 export const Login = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const isUsernameValid = username.length >= 3
+
+    const isPasswordValid = checkPassword(password)
+
+
+    const isValid = isUsernameValid && isPasswordValid
+
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -12,19 +25,19 @@ export const Login = () => {
                             <label className="label">
                                 <span className="label-text">Username</span>
                             </label>
-                            <input type="text" placeholder="username" className="input input-bordered" required />
+                            <input onChange={(e) => setUsername(e.target.value)} type="text" placeholder="username" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" className="input input-bordered" required />
+                            <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder="password" className="input input-bordered" required />
                             <label className="label"> Don{"'"}t have an account?
                                 <a href="#/signup" className="label-text-alt link link-hover">Sign up</a>
                             </label>
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
+                            <button disabled={!isValid} className="btn btn-primary">Login</button>
                         </div>
                     </form>
                 </div>
