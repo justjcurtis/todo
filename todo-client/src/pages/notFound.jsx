225e8194efa4c4ab2c941a558daac13be84a14cc
justjcurtis <justjcurtis@gmail.com>
2023-12-11 09:07:36 +0000

    feat: add not found page

--- todo-client/src/pages/notFound.jsx
+++ todo-client/src/pages/notFound.jsx
@@ -1,6 +1,14 @@
 export const NotFound = () => {
     return (
-        <>
-        </>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="text-center hero-content">
+                <div className="max-w-md">
+                    <h1 className="mb-5 text-5xl font-bold">
+                        404
+                    </h1>
+                    <p className="mb-5">Page not found. <a href="/">Go home</a></p>
+                </div>
+            </div>
+        </div>
     )
 }
