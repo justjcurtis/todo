0c161e22e318578154972faafb54dd2935de37e6
justjcurtis <justjcurtis@gmail.com>
2023-12-11 06:56:23 +0000

    fix: typos for delete request

--- todo-client/src/api/deleteTodoRequest.js
+++ todo-client/src/api/deleteTodoRequest.js
@@ -2,7 +2,7 @@ import { API_URL } from '../constants'
 import axios from 'axios'
 export const deleteTodoRequest = async (id) => {
     try {
-        const response = await axios.delete(`${API_URL}/todo?id=${id}`)
+        const response = await axios.post(`${API_URL}/deleteTodo?id=${id}`)
         return response.data
     } catch (err) {
         console.error(err.response.data.error)
