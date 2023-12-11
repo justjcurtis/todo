3017032e5ad7347945a7864962d82571b8fbdb45
justjcurtis <justjcurtis@gmail.com>
2023-12-11 00:55:13 +0000

    feat: added placeholder to new todo component

--- todo-client/src/components/newTodo.jsx
+++ todo-client/src/components/newTodo.jsx
@@ -28,7 +28,7 @@ export const NewTodo = ({ onCreate }) => {
     return (
         <div className='flex'>
             <input onChange={() => setCompleted(!completed)} type="checkbox" checked={completed ? "checked" : ""} className="checkbox checkbox-accent my-auto" />
-            <input onChange={(e) => setText(e.target.value)} value={text} type="text" className="input input-bordered w-full mx-5 my-1" />
+            <input onChange={(e) => setText(e.target.value)} placeholder='Add a new todo' value={text} type="text" className="input input-bordered w-full mx-5 my-1" />
             <button onClick={handleAddPressed} style={styles.buttonStyle} className={`btn btn-circle my-auto text-secondary-content bg-accent hover:bg-accent ${buttonClasses}`}>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
             </button>
