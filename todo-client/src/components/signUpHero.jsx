45afc05deca2e67551ee0ef19fbc42b36656d382
justjcurtis <justjcurtis@gmail.com>
2023-12-11 01:02:29 +0000

    chore: updated SignupHero text

--- todo-client/src/components/signUpHero.jsx
+++ todo-client/src/components/signUpHero.jsx
@@ -5,7 +5,7 @@ export const SignUpHero = () => {
                 <div className="max-w-md">
                     <h1 className="text-5xl font-bold">Hello there THM</h1>
                     <p className="pt-6">Welcome to todo. A simple todo app for all your todoing needs.</p>
-                    <p className="pb-6">Please sign up using the button below to do the todos you are yet to do.</p>
+                    <p className="pb-6">Please sign up using the button below to get started on doing the todos you are yet to do.</p>
                     <a href="/#signup" className="btn btn-primary">Get Started</a>
                 </div>
             </div>
